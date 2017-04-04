72347a0b5144a0f8c7539589f3ea752803dc47cd
Santiago Ramos <sramos@oei.es>
2017-04-04 17:20:00 +0200

    Slug created

--- app/models/url.rb
+++ app/models/url.rb
@@ -13,5 +13,10 @@ class Url < ApplicationRecord
   end
 
   def create_slug
+    slug_length = 6
+    while slug.nil?
+      temptative = SecureRandom.random_number(36**slug_length).to_s(36).rjust(slug_length, "0")
+      slug = temptative if Url.find_by_slug.empty?
+    end
   end
 end
